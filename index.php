12f7b08d4cc19f2efe38ad96d785df8c1b4eab7b
ClementG60 <cle_guillaume@hotmail.fr>
2021-12-07 12:05:33 +0100

    ajout fichier

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        $name = 'Clement';
+        echo 'Bonjour ' . $name . ' comment vas-tu ?'
+        ?>
+    </p>
+</body>
+</html>
